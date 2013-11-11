05d046bb7b15cfc30e69bd8149e3c95ae62218a9
duanbn <duanbn@126.com>
2013-11-11 09:51:32 +0800

    收录服务端程序

---
+++ src/main/java/com/dy/webmark/controller/MarkController.java
@@ -0,0 +1,31 @@
+package com.dy.webmark.controller;
+
+import javax.servlet.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+import org.apache.log4j.*;
+
+@Controller
+@RequestMapping("/mark")
+public class MarkController extends BaseController {
+
+    public static final Logger LOG = Logger.getLogger(MarkController.class);
+
+    public static final String SHOULU = "shoulu";
+
+    @RequestMapping("/showdlg.do")
+    public String showMarkDialog(HttpServletRequest req, HttpServletResponse resp) {
+        String title = req.getParameter("title");
+        String url = req.getParameter("url");
+        String keywords = req.getParameter("keywords");
+        String desc = req.getParameter("desc");
+
+        req.setAttribute("title", title);
+        req.setAttribute("url", url);
+        req.setAttribute("keywords", keywords);
+        req.setAttribute("desc", desc);
+
+        return SHOULU;
+    }
+
+}
